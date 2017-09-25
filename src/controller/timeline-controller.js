137823fc25afd430f86c6ee8eb0913efc6485e08
Bartlomiej Obecny <obecny@interia.pl>
2017-09-25 17:17:22 +0200

    cleanups

--- src/controller/timeline-controller.js
+++ src/controller/timeline-controller.js
@@ -278,23 +278,23 @@ class TimelineController extends EventHandler {
 
         // Parse the WebVTT file contents.
         WebVTTParser.parse(payload, this.initPTS, vttCCs, frag.cc, function (cues) {
-          const currentTrack = textTracks[frag.trackId];
-          // Add cues and trigger event with success true.
-          cues.forEach(cue => {
-            // Sometimes there are cue overlaps on segmented vtts so the same
-            // cue can appear more than once in different vtt files.
-            // This avoid showing duplicated cues with same timecode and text.
-            if (!currentTrack.cues.getCueById(cue.id)) {
-              try {
-                currentTrack.addCue(cue);
-              } catch (err) {
-                const textTrackCue = new window.TextTrackCue(cue.startTime, cue.endTime, cue.text);
-                textTrackCue.id = cue.id;
-                currentTrack.addCue(textTrackCue);
+            const currentTrack = textTracks[frag.trackId];
+            // Add cues and trigger event with success true.
+            cues.forEach(cue => {
+              // Sometimes there are cue overlaps on segmented vtts so the same
+              // cue can appear more than once in different vtt files.
+              // This avoid showing duplicated cues with same timecode and text.
+              if (!currentTrack.cues.getCueById(cue.id)) {
+                try {
+                  currentTrack.addCue(cue);
+                } catch (err) {
+                  const textTrackCue = new window.TextTrackCue(cue.startTime, cue.endTime, cue.text);
+                  textTrackCue.id = cue.id;
+                  currentTrack.addCue(textTrackCue);
+                }
               }
-            }
-          });
-          hls.trigger(Event.SUBTITLE_FRAG_PROCESSED, {success: true, frag: frag});
+            });
+            hls.trigger(Event.SUBTITLE_FRAG_PROCESSED, {success: true, frag: frag});
         },
         function (e) {
           // Something went wrong while parsing. Trigger event with success false.
