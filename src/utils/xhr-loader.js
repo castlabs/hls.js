00da0182963e373045fc042dec84b9abe167e2ba
Tomasz Oponowicz <tomasz-oponowicz@users.noreply.github.com>
2017-11-27 12:04:45 +0100

    Send an entrie context of a request to xhrSetup and apply changes (#1)
    * Send an entrie context of a request to xhrSetup and apply changes
    
    * Ignore lint error
    
    * Fix IE 11 error

--- src/utils/xhr-loader.js
+++ src/utils/xhr-loader.js
@@ -42,42 +42,54 @@ class XhrLoader {
   loadInternal() {
     var xhr, context = this.context;
     xhr = this.loader = new XMLHttpRequest();
-    
+
     let stats = this.stats;
     stats.tfirst = 0;
     stats.loaded = 0;
     const xhrSetup = this.xhrSetup;
 
+    const mutableContext = {
+      xhr: xhr,
+      method: 'GET',
+      uris: [context.url],
+      headers: {},
+      allowCrossSiteCredentials: false,
+      body: undefined
+    };
+
+    if (context.rangeEnd) {
+      /*jshint -W069 */
+      mutableContext.headers['Range'] = 'bytes=' + context.rangeStart + '-' + (context.rangeEnd - 1);
+      /*jshint +W069 */
+    }
+
     try {
       if (xhrSetup) {
-        try {
-          xhrSetup(xhr, context.url);
-        } catch (e) {
-          // fix xhrSetup: (xhr, url) => {xhr.setRequestHeader("Content-Language", "test");}
-          // not working, as xhr.setRequestHeader expects xhr.readyState === OPEN
-          xhr.open('GET', context.url, true);
-          xhrSetup(xhr, context.url);
-        }
+        xhrSetup(mutableContext);
       }
-      if (!xhr.readyState) {
-        xhr.open('GET', context.url, true);
-      }
+
+      xhr.open(mutableContext.method, mutableContext.uris[0], true);
     } catch (e) {
       // IE11 throws an exception on xhr.open if attempting to access an HTTP resource over HTTPS
       this.callbacks.onError({ code : xhr.status, text: e.message }, context, xhr);
       return;
     }
 
-    if (context.rangeEnd) {
-      xhr.setRequestHeader('Range','bytes=' + context.rangeStart + '-' + (context.rangeEnd-1));
-    }
+    Object.keys(mutableContext.headers).forEach(function(name) {
+      var value = mutableContext.headers[name];
+      xhr.setRequestHeader(name, value);
+    });
+
+    xhr.allowCrossSiteCredentials = mutableContext.allowCrossSiteCredentials;
+
+    xhr.responseType = context.responseType;
+
     xhr.onreadystatechange = this.readystatechange.bind(this);
     xhr.onprogress = this.loadprogress.bind(this);
-    xhr.responseType = context.responseType;
 
     // setup timeout before we perform request
     this.requestTimeout = window.setTimeout(this.loadtimeout.bind(this), this.config.timeout);
-    xhr.send();
+    xhr.send(mutableContext.body);
   }
 
   readystatechange(event) {
