b6ac1d416b0e12204b6448abe7b5851f63edfcf4
Bartłomiej Makowski <biuro@mackode.pl>
2019-07-10 07:36:59 +0200

    Revert "DE-4613: fix buffering issues regarding device memory limitation"
    This reverts commit 3a52bf21ac40629697ed2ad2e77427fc9995b101.

--- src/loader/playlist-loader.js
+++ src/loader/playlist-loader.js
@@ -49,6 +49,10 @@ class Fragment {
     this._byteRange = null;
     this._decryptdata = null;
     this.tagList = [];
+
+    this.cachedData = null;
+    this.cacheInProgress = false;
+    this.cacheStats = null;
   }
 
   get url() {
