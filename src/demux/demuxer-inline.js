629fe62cb929ad19f0c3addee19ca49d4a5dda8d
Armand Zangue <armand.zangue@gmail.com>
2020-03-15 22:01:21 +0100

    DE-5158: Fix clear HLS not working on Chrome/FF
    Changed demuxer probing order. Prioritize MP4 demuxer to avoid false
    detection with AAC/MP3 Demuxer. (hls.js)

--- src/demux/demuxer-inline.js
+++ src/demux/demuxer-inline.js
@@ -66,12 +66,12 @@ class DemuxerInline {
       const observer = this.observer;
       const typeSupported = this.typeSupported;
       const config = this.config;
-      // probing order is TS/AAC/MP3/MP4
+      // probing order is TS/MP4/AAC/MP3
       const muxConfig = [
         { demux: TSDemuxer, remux: MP4Remuxer },
+        { demux: MP4Demuxer, remux: PassThroughRemuxer },
         { demux: AACDemuxer, remux: MP4Remuxer },
-        { demux: MP3Demuxer, remux: MP4Remuxer },
-        { demux: MP4Demuxer, remux: PassThroughRemuxer }
+        { demux: MP3Demuxer, remux: MP4Remuxer }
       ];
 
       // probe for content type
